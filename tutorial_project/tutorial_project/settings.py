769c9af051cb597e63ffc7fd56c0c8504418ead0
Dtagoe125 <dtagoe125@gmail.com>
2016-10-28 14:04:04 -0400

    commit

--- tutorial_project/tutorial_project/settings.py
+++ tutorial_project/tutorial_project/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR,'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+    )
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
