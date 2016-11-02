b3bf5d57073ffb16234621a4a130f06ee329aa64
Dtagoe125 <dtagoe125@gmail.com>
2016-11-01 20:59:34 -0400

    Class November1

--- tutorial_project/tutorial_app/admin.py
+++ tutorial_project/tutorial_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from models import Category, Page
+from models import Category, Page, UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
+admin.site.register(UserProfile)
 
 
 # Register your models here.
--- tutorial_project/tutorial_app/views.py
+++ tutorial_project/tutorial_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from models import Page
-from forms import CategoryForm, PageForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from models import Page, Category
+from forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -89,3 +92,84 @@ def add_page(request, category_name_slug):
 	return render(request, 'add_page.html', context_dict)
 
 
+def register(request):
+	registered = False
+	if request.method =='POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+
+			user.set_password(user.password)
+			user.save()
+
+			profile = profile_form.save(commit=False)
+			profile.user = user
+
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+
+			profile.save()
+
+			registered = True
+		else:
+			print user_form.errors, profile_form.errors
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+	return render(request,
+		'register.html',
+		{'user_form': user_form,
+		 'profile_form': profile_form, 
+		 'registered': registered} 
+		  )
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if user:
+				if user.is_active:
+					login(request, user)
+					return HttpResponseRedirect('/')
+				else:
+					return HttpResponse("Your account is disabled.")
+		else:
+			print "Invalid login details: {0}, {1}".format(username, password)
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'login.html', {})
+
+@login_required
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			form = CategoryForm()
+
+		return render(request, 'add_category.html', {'form':form})
+
+@login_required
+def add_page(request):
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			form = PageForm()
+
+		return render(request, 'add_page.html', {'form':form})
+
+@login_required
+def user_logout(request):
+	logout(request)
+
+	return HttpResponseRedirect('/')
